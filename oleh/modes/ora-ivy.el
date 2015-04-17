932843dabb7c18ba2147a7853c70872fd0474993
Oleh Krehel <ohwoeowho@gmail.com>
2015-04-17 10:54:04 +0200

    oleh/modes/ora-ivy.el: Bind "<return>"

--- oleh/modes/ora-ivy.el
+++ oleh/modes/ora-ivy.el
@@ -27,6 +27,7 @@ _h_ ^✜^ _l_     _r_eplace    _,_ unmark   _o_: quit
   ("i" nil))
 
 (define-key ivy-minibuffer-map (kbd "C-o") 'hydra-ivy/body)
+(define-key ivy-minibuffer-map (kbd "<return>") 'ivy-alt-done)
 
 (defun ivy-dired-mark (arg)
   (interactive "p")
