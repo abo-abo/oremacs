a462d06a4ff0be3a708f36cab166bddbc446125a
Oleh Krehel <ohwoeowho@gmail.com>
2015-07-29 15:16:40 +0200

    oleh/modes/ora-org.el (counsel-org-tag): Bind to "C-c C-q"

--- oleh/modes/ora-org.el
+++ oleh/modes/ora-org.el
@@ -26,6 +26,7 @@
 (define-key org-mode-map (kbd "C-a") 'ora-move-beginning-of-line)
 (define-key org-mode-map (kbd "M-r") 'org-ctrl-c-ctrl-c)
 (define-key org-src-mode-map (kbd "C-c C-c") 'org-edit-src-exit)
+(define-key org-mode-map (kbd "C-c C-q") 'counsel-org-tag)
 
 ;;** org-agenda-mode-map
 (require 'org-agenda)
