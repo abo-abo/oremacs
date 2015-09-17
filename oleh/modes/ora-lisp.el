04b4ea6ac6d1a25eecc233ef9a743ef23bf71fc0
Oleh Krehel <ohwoeowho@gmail.com>
2015-09-17 12:00:00 +0300

    ora-lisp.el (lisp-mode-map): Bind "β" to counsel-cl

--- oleh/modes/ora-lisp.el
+++ oleh/modes/ora-lisp.el
@@ -1,6 +1,7 @@
 (require 'slime)
 (define-key slime-mode-map (kbd "M-p") nil)
 (define-key slime-mode-map (kbd "C-M-i") nil)
+(define-key lisp-mode-map (kbd "β") 'counsel-cl)
 
 ;;;###autoload
 (defun ora-lisp-hook ()
