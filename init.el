8f62af463f227a3463e5ad108299f7c8a68533a5
Oleh Krehel <ohwoeowho@gmail.com>
2016-02-08 10:31:22 +0100

    init.el (custom-theme-load-confirm): Don't confirm safe themes

--- init.el
+++ init.el
@@ -212,6 +212,8 @@
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
   "Prevent annoying \"Active processes exist\" query when you quit Emacs."
   (lispy-flet (process-list ()) ad-do-it))
+(defadvice custom-theme-load-confirm (around no-query-safe-thme activate)
+  t)
 (use-package dired
     :commands dired
     :init
