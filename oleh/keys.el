8ab3f253e8fb8c05d71ee57e632d9ed93e31639a
Oleh Krehel <ohwoeowho@gmail.com>
2016-03-12 10:34:13 +0100

    oleh/keys.el: Update "C-c s"

--- oleh/keys.el
+++ oleh/keys.el
@@ -112,7 +112,7 @@
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c h") 'hydra-apropos/body)
-(global-set-key (kbd "C-c s") 'isearch-forward)
+(global-set-key (kbd "C-c s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-c r") 'isearch-backward)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 ;;* C-x shortcuts
