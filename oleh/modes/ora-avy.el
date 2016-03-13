85b18a1777c357c6459a3a5c86ca8c9406536584
Oleh Krehel <ohwoeowho@gmail.com>
2016-03-13 16:59:39 +0100

    oleh/modes/ora-avy.el (hydra-avy): Add

--- oleh/modes/ora-avy.el
+++ oleh/modes/ora-avy.el
@@ -11,9 +11,19 @@
 (setq avy-styles-alist '((avy-goto-char-2 . post)
                          (ivy-avy . pre)
                          (avy-goto-line . pre)))
+(advice-add 'swiper :before 'avy-push-mark)
+;; (advice-remove 'swiper 'avy-push-mark)
 
 ;;* Lispy
 (setq avy-keys-alist
       `((lispy-ace-symbol . ,aw-keys)))
+
+(defhydra hydra-avy (:color teal)
+  ("j" avy-goto-char)
+  ("k" avy-goto-word-1)
+  ("l" avy-goto-line)
+  ("s" avy-goto-char-timer)
+  ("f" counsel-find-file)
+  ("q" nil))
 
 (provide 'ora-avy)
