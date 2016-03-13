2f6b33f9300ffd651383bce891b0d5018076e351
Oleh Krehel <ohwoeowho@gmail.com>
2016-03-13 16:57:30 +0100

    oleh/modes/ora-pdf-view.el: Rebind "C-s"

--- oleh/modes/ora-pdf-view.el
+++ oleh/modes/ora-pdf-view.el
@@ -2,6 +2,7 @@
 
 (define-key pdf-view-mode-map "j" 'pdf-view-next-line-or-next-page)
 (define-key pdf-view-mode-map "k" 'pdf-view-previous-line-or-previous-page)
+(define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward-regexp)
 
 ;;;###autoload
 (defun ora-pdf-view-hook ())
