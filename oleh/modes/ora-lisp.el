07c8c5d78f811f7574b91c97cfc536a3248c3942
Oleh Krehel <ohwoeowho@gmail.com>
2016-03-13 17:04:32 +0100

    oleh/modes/ora-lisp.el: Unbind "C-M-i"

--- oleh/modes/ora-lisp.el
+++ oleh/modes/ora-lisp.el
@@ -1,6 +1,7 @@
 (require 'slime)
 (define-key slime-mode-map (kbd "M-p") nil)
 (define-key slime-mode-map (kbd "C-M-i") nil)
+(define-key slime-mode-indirect-map (kbd "C-M-i") nil)
 (define-key lisp-mode-map (kbd "β") 'counsel-cl)
 (setq lispy-colon-no-space-regex nil)
 
