2936837d54309d1233dff605a652dcfcac45e875
Oleh Krehel <ohwoeowho@gmail.com>
2016-03-14 18:23:48 +0100

    oleh/modes/ora-dired.el (ora-dired-start-process): Adapt to Windows

--- oleh/modes/ora-dired.el
+++ oleh/modes/ora-dired.el
@@ -62,21 +62,25 @@
    (let ((files (dired-get-marked-files
                  t current-prefix-arg)))
      (list
-      (dired-read-shell-command "& on %s: "
-                                current-prefix-arg files)
+      (unless (eq system-type 'windows-nt)
+        (dired-read-shell-command "& on %s: "
+                                  current-prefix-arg files))
       files)))
-  (let (list-switch)
-    (start-process
-     cmd nil shell-file-name
-     shell-command-switch
-     (format
-      "nohup 1>/dev/null 2>/dev/null %s \"%s\""
-      (if (and (> (length file-list) 1)
-               (setq list-switch
-                     (cadr (assoc cmd ora-dired-filelist-cmd))))
-          (format "%s %s" cmd list-switch)
-        cmd)
-      (mapconcat #'expand-file-name file-list "\" \"")))))
+  (if (eq system-type 'windows-nt)
+      (dolist (file file-list)
+        (w32-shell-execute "open" (expand-file-name file)))
+    (let (list-switch)
+      (start-process
+       cmd nil shell-file-name
+       shell-command-switch
+       (format
+        "nohup 1>/dev/null 2>/dev/null %s \"%s\""
+        (if (and (> (length file-list) 1)
+                 (setq list-switch
+                       (cadr (assoc cmd ora-dired-filelist-cmd))))
+            (format "%s %s" cmd list-switch)
+          cmd)
+        (mapconcat #'expand-file-name file-list "\" \""))))))
 
 (defun ora-dired-open-term ()
   "Open an `ansi-term' that corresponds to current directory."
