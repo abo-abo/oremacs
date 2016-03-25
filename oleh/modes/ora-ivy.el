6ab01c059a8ca5babeeeaeb9eed282baa503a470
Oleh Krehel <ohwoeowho@gmail.com>
2016-03-25 08:40:28 +0100

    oleh/modes/ora-ivy.el: Update

--- oleh/modes/ora-ivy.el
+++ oleh/modes/ora-ivy.el
@@ -10,7 +10,7 @@
 (define-key ivy-minibuffer-map (kbd "C-M-h") 'ivy-previous-line-and-call)
 (define-key ivy-minibuffer-map (kbd "C-:") 'ivy-dired)
 (define-key ivy-minibuffer-map (kbd "C-c o") 'ivy-occur)
-(when (and (version< "24.5" emacs-version)
+(when (and (version< "25" emacs-version)
            (eq system-type 'gnu/linux))
   (diminish 'ivy-mode " 🙒"))
 
