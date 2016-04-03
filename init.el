deedfcfd1d86dc3ff191c102a5d3bdc9a361359f
Oleh Krehel <ohwoeowho@gmail.com>
2016-04-03 12:33:05 +0200

    init.el: Update

--- init.el
+++ init.el
@@ -144,13 +144,13 @@
     :diminish yas-minor-mode
     :config
     (progn
+      (setq yas-after-exit-snippet-hook '((lambda () (yas-global-mode -1))))
       (setq yas-fallback-behavior 'return-nil)
       (setq yas-triggers-in-field t)
       (setq yas-verbosity 0)
       (setq yas-snippet-dirs (list (concat emacs-d "snippets/")))
       (define-key yas-minor-mode-map [(tab)] nil)
-      (define-key yas-minor-mode-map (kbd "TAB") nil)
-      (yas-global-mode)))
+      (define-key yas-minor-mode-map (kbd "TAB") nil)))
 (use-package auto-yasnippet
     :commands aya-create aya-open-line)
 (use-package abel)
