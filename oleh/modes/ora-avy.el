e9f0d441bfdec79d5055f1ab52566c17dc4e57d2
Oleh Krehel <ohwoeowho@gmail.com>
2016-04-06 12:40:56 +0200

    oleh/modes/ora-avy.el: Update

--- oleh/modes/ora-avy.el
+++ oleh/modes/ora-avy.el
@@ -1,22 +1,22 @@
 ;;* Ace Window
 (require 'ace-window)
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-(setq aw-background nil)
+(csetq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(csetq aw-background nil)
 (csetq aw-flip-keys '("n" "ν"))
 (add-to-list 'aw-dispatch-alist '(?ν aw-flip-window))
 
 ;;* Avy
 (avy-setup-default)
-(setq avy-all-windows nil)
-(setq avy-styles-alist '((avy-goto-char-2 . post)
-                         (ivy-avy . pre)
-                         (avy-goto-line . pre)))
+(csetq avy-all-windows nil)
+(csetq avy-styles-alist '((avy-goto-char-2 . post)
+                          (ivy-avy . pre)
+                          (avy-goto-line . pre)))
 (advice-add 'swiper :before 'avy-push-mark)
 ;; (advice-remove 'swiper 'avy-push-mark)
 
 ;;* Lispy
-(setq avy-keys-alist
-      `((lispy-ace-symbol . ,aw-keys)))
+(csetq avy-keys-alist
+       `((lispy-ace-symbol . ,aw-keys)))
 
 (defhydra hydra-avy (:color teal)
   ("j" avy-goto-char)
