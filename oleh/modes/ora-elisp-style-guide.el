1f9f610ff55565b1852a23f140fa3abf1ff629d8
Oleh Krehel <ohwoeowho@gmail.com>
2016-05-11 15:06:50 +0200

    oleh/modes/ora-elisp-style-guide.el: Update

--- oleh/modes/ora-elisp-style-guide.el
+++ oleh/modes/ora-elisp-style-guide.el
@@ -1,15 +1,14 @@
 (setq indent-tabs-mode nil)
-(put 'defface 'lisp-indent-function 1)
-(put 'define-minor-mode 'lisp-indent-function 1)
-(eval-after-load 'cl-indent
-  `(progn
-     (put 'cl-flet 'common-lisp-indent-function
-          (get 'flet 'common-lisp-indent-function))
-     (put 'cl-labels 'common-lisp-indent-function
-          (get 'labels 'common-lisp-indent-function))
-     (put 'if 'common-lisp-indent-function 2)))
-
-(setq lisp-indent-function 'common-lisp-indent-function)
+(require 'cl-indent)
 ;; (setq lisp-indent-function 'lisp-indent-function)
+(setq lisp-indent-function 'common-lisp-indent-function)
+
+(put 'defface 'common-lisp-indent-function 1)
+(put 'define-minor-mode 'common-lisp-indent-function 1)
+(put 'define-derived-mode 'common-lisp-indent-function 3)
+(put 'cl-flet 'common-lisp-indent-function
+     (get 'flet 'common-lisp-indent-function))
+(put 'cl-labels 'common-lisp-indent-function
+     (get 'labels 'common-lisp-indent-function))
 
 (provide 'ora-elisp-style-guide)
