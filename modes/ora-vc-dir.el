886ffab2716ccd2c8533b67743591c98c58c28a9
Oleh Krehel <ohwoeowho@gmail.com>
2016-05-30 17:01:48 +0200

    modes/ora-vc-dir.el: Bind vc-revert to "r"

--- modes/ora-vc-dir.el
+++ modes/ora-vc-dir.el
@@ -3,7 +3,7 @@
 (define-key vc-dir-mode-map "j" 'vc-dir-next-line)
 (define-key vc-dir-mode-map "k" 'vc-dir-previous-line)
 (define-key vc-dir-mode-map "e" 'vc-ediff)
-(define-key vc-dir-mode-map "R" 'vc-revert)
+(define-key vc-dir-mode-map "r" 'vc-revert)
 
 ;;;###autoload
 (defun ora-vc-dir-hook ())
