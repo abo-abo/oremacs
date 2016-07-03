9d05b86fe0db3d064fbea5e6303bcd17c12896f2
Oleh Krehel <ohwoeowho@gmail.com>
2016-07-03 08:20:08 +0200

    modes/ora-mu4e.el: Update

--- modes/ora-mu4e.el
+++ modes/ora-mu4e.el
@@ -1,4 +1,9 @@
+(add-to-list 'load-path "~/git/mu/mu4e")
 (require 'mu4e)
+(setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+(setq mu4e-headers-skip-duplicates t)
 
 (define-key mu4e-headers-mode-map "h" 'mu4e-headers-prev)
 
