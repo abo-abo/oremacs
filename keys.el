54ce56e67f533daaf0b6e5093e58c9f16bcf232a
Oleh Krehel <ohwoeowho@gmail.com>
2016-07-18 07:43:57 +0200

    keys.el: Update

--- keys.el
+++ keys.el
@@ -119,8 +119,8 @@
 (global-set-key (kbd "C-c r") 'isearch-backward)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "C-c m") 'counsel-linux-app)
-(global-set-key (kbd "C-c o") 'hydra-org-objects/body)
 (global-set-key (kbd "C-c v") 'vc-ediff)
+(global-set-key (kbd "C-c o") nil)
 (global-set-key (kbd "C-c w") 'ora-org-to-html-to-clipboard)
 (global-set-key (kbd "C-c D") 'ora-insert-date-from)
 
