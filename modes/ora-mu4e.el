8a07a32b6feee243c632ba70c8ab8f821ad757c2
Oleh Krehel <ohwoeowho@gmail.com>
2016-08-31 11:23:06 +0200

    modes/ora-mu4e.el: Update

--- modes/ora-mu4e.el
+++ modes/ora-mu4e.el
@@ -4,8 +4,21 @@
 (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
 (setq mu4e-trash-folder  "/[Gmail].Trash")
 (setq mu4e-headers-skip-duplicates t)
+;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+(setq mu4e-sent-messages-behavior 'delete)
+(setq mu4e-get-mail-command "offlineimap")
+(setq message-kill-buffer-on-exit t)
 
-(define-key mu4e-headers-mode-map "h" 'mu4e-headers-prev)
+;; setup some handy shortcuts
+;; you can quickly switch to your Inbox -- press ``ji''
+;; then, when you want archive some messages, move them to
+;; the 'All Mail' folder by pressing ``ma''.
+(setq mu4e-maildir-shortcuts
+      '(("/INBOX" . ?i)
+        ("/[Gmail].Sent Mail" . ?s)
+        ("/[Gmail].Trash" . ?t)
+        ("/[Gmail].All Mail" . ?a)
+        ("/[asml].All Mail" . ?m)))
 
 ;;;###autoload
 (defun ora-mu4e-headers-hook ())
