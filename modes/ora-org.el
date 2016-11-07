61e9e8e0a1993e1e793a0fab8a15a4cca663530f
Oleh Krehel <ohwoeowho@gmail.com>
2016-11-07 14:37:34 +0100

    modes/ora-org.el: Update key bindings for agenda

--- modes/ora-org.el
+++ modes/ora-org.el
@@ -61,18 +61,59 @@
 ;;** org-agenda-mode-map
 (require 'org-agenda)
 
-(define-key org-agenda-mode-map "p" 'ora-org-pomodoro)
-(define-key org-agenda-mode-map (kbd "C-j") 'org-open-at-point)
-(define-key org-agenda-mode-map "i" 'org-agenda-clock-in)
-(define-key org-agenda-mode-map "o" 'org-agenda-clock-out)
-(define-key org-agenda-mode-map "0" 'ora-org-schedule-today)
-(define-key org-agenda-mode-map "1" 'ora-org-schedule-tomorrow)
-;; was `org-agenda-goto-date'
-(define-key org-agenda-mode-map "j" 'org-agenda-next-line)
-;; was `org-agenda-capture'
-(define-key org-agenda-mode-map "k" 'org-agenda-previous-line)
-(define-key org-agenda-mode-map "v" 'hydra-org-agenda-view/body)
-(define-key org-agenda-mode-map "T" 'worf-clock-in-and-out)
+(let ((map org-agenda-mode-map))
+  ;; unbind
+  (define-key map "a" 'worf-reserved)
+  (define-key map "b" 'worf-reserved)
+  (define-key map "c" 'worf-reserved)
+  (define-key map "d" 'worf-reserved)
+  (define-key map "e" 'worf-reserved)
+  (define-key map "f" 'worf-reserved)
+  (define-key map "n" 'worf-reserved)
+  (define-key map "o" 'worf-reserved)
+  (define-key map "u" 'worf-reserved)
+  (define-key map "w" 'worf-reserved)
+  (define-key map "y" 'worf-reserved)
+  (define-key map "z" 'worf-reserved)
+  ;; arrows
+  (define-key map "j" 'org-agenda-next-item)
+  (define-key map "k" 'org-agenda-previous-item)
+  (define-key map "h" 'org-agenda-earlier)
+  (define-key map "l" 'org-agenda-later)
+  ;; worf
+  (define-key map "s" 'worf-schedule)
+  (define-key map "N" 'worf-agenda-narrow)
+  (define-key map "W" 'worf-agenda-widen)
+  (define-key map "t" 'worf-todo)
+  ;; misc
+  (define-key map "p" 'ora-org-pomodoro)
+  (define-key map (kbd "C-j") 'org-open-at-point)
+  (define-key map "i" 'org-agenda-clock-in)
+  (define-key map "o" 'org-agenda-clock-out)
+  (define-key map "0" 'digit-argument)
+  (define-key map "1" 'digit-argument)
+  (define-key map "v" 'hydra-org-agenda-view/body)
+  (define-key map "x" 'hydra-org-agenda-ex/body)
+  (define-key map "S" 'org-save-all-org-buffers)
+  (define-key map "T" 'worf-clock-in-and-out)
+  ;; disable
+  (define-key map "f" nil))
+
+(defhydra hydra-org-agenda-ex (:color blue :columns 2)
+  "x"
+  ("a" org-agenda-archive-default-with-confirmation "archive")
+  ("b" org-agenda-earlier "earlier")
+  ("c" org-agenda-goto-calendar "calendar")
+  ("e" org-agenda-set-effort "effort")
+  ("h" org-agenda-holidays "holidays")
+  ("i" org-agenda-diary-entry "diary entry")
+  ("j" org-agenda-goto-date "goto date")
+  ("k" org-agenda-capture "capture")
+  ("l" org-agenda-log-mode "log-mode")
+  ("o" delete-other-windows "one window")
+  ("r" org-agenda-redo "redo")
+  ("u" org-agenda-bulk-unmark "unmark")
+  ("z" org-agenda-add-note "note"))
 
 ;;* Basic settings
 (setq-default org-todo-keywords
