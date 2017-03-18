54b32def82d4a4000e19df4f6cc8a7e1489702df
Oleh Krehel <ohwoeowho@gmail.com>
2017-03-18 22:02:51 +0100

    modes/ora-scheme.el: Update to new Geiser

--- modes/ora-scheme.el
+++ modes/ora-scheme.el
@@ -1,13 +1,15 @@
 (require 'geiser-mode)
 (define-key geiser-mode-map (kbd "C-.") nil)
 (define-key geiser-mode-map (kbd "C-c C-l") 'geiser-eval-buffer)
-(setq geiser-impl--implementation 'racket)
+(setq geiser-active-implementations '(racket))
 
 ;;;###autoload
 (defun ora-scheme-hook ()
   (setq prettify-symbols-alist
         '(("lambda" . ?λ)))
+  (setq-local lispy-outline-header ";")
   (prettify-symbols-mode)
+  (geiser-mode)
   (lispy-mode)
   (company-mode -1))
 
