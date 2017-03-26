444ad241de30d15304d56f9bc703819f7a167111
Oleh Krehel <ohwoeowho@gmail.com>
2017-03-26 14:12:56 +0200

    modes/ora-scheme.el (scheme-completion-at-point): Add

--- modes/ora-scheme.el
+++ modes/ora-scheme.el
@@ -4,14 +4,21 @@
 (setq geiser-active-implementations '(racket))
 (setq geiser-mode-autodoc-p nil)
 
+(defun scheme-completion-at-point ()
+  (let* ((bnd (bounds-of-thing-at-point 'symbol))
+         (prefix (lispy--string-dwim bnd)))
+    (with-current-buffer (geiser-repl--buffer-name geiser-impl--implementation)
+      (list (car bnd) (cdr bnd)
+            (geiser-completion--symbol-list prefix)))))
 ;;;###autoload
 (defun ora-scheme-hook ()
   (setq prettify-symbols-alist
         '(("lambda" . ?λ)))
-  (setq-local lispy-outline-header ";")
+  (setq-local lispy-outline-header ";;")
   (prettify-symbols-mode)
   (geiser-mode)
   (lispy-mode)
-  (company-mode -1))
+  (company-mode -1)
+  (setq-local completion-at-point-functions '(scheme-completion-at-point)))
 
 (setq scheme-program-name "/usr/bin/csi")
