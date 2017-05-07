229f0d39f2c800a2a3053b113897389d25df7ce3
Oleh Krehel <ohwoeowho@gmail.com>
2017-05-07 10:20:44 +0200

    modes/ora-doc-view.el (doc-view-evince): Add and bind to "x"

--- modes/ora-doc-view.el
+++ modes/ora-doc-view.el
@@ -5,6 +5,11 @@
 (define-key doc-view-mode-map "\C-p" 'doc-view-previous-page)
 (define-key doc-view-mode-map "k" 'doc-view-previous-page)
 (define-key doc-view-mode-map "j" 'doc-view-next-page)
+(define-key doc-view-mode-map "x" 'doc-view-evince)
+
+(defun doc-view-evince ()
+  (interactive)
+  (ora-dired-start-process "evince" (list (buffer-file-name))))
 
 ;;;###autoload
 (defun ora-doc-view-hook ())
