571bce10fb859342fa16f16b344bad16cf2ec097
Oleh Krehel <ohwoeowho@gmail.com>
2017-09-05 19:13:26 +0200

    modes/ora-eshell.el: Update

--- modes/ora-eshell.el
+++ modes/ora-eshell.el
@@ -1,5 +1,22 @@
 (require 'eshell)
 
+(defun eshell-completion-at-point ()
+  (let* ((t1 (comint-completion-at-point))
+         (beg (nth 0 t1))
+         (end (nth 1 t1))
+         (str (buffer-substring-no-properties beg end))
+         (collection (nth 2 t1))
+         (comps
+          (completion-all-completions str collection predicate (- end beg))))
+    (if comps
+        t1
+      (elisp-completion-at-point))))
+
+;;;###autoload
+(defun ora-eshell-hook ()
+  (define-key eshell-mode-map (kbd "<tab>") 'completion-at-point)
+  (setq completion-at-point-functions '(eshell-completion-at-point)))
+
 ;;;###autoload
 (defun eshell-this-dir ()
   "Open or move eshell in `default-directory'."
