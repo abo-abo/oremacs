d84622993c3a1e4ea650f7da3c03489a0572ccc0
Oleh Krehel <ohwoeowho@gmail.com>
2017-12-28 20:49:21 +0100

    modes/ora-company.el (ora-company-number): Add

--- modes/ora-company.el
+++ modes/ora-company.el
@@ -19,13 +19,29 @@
         company-files
         company-dabbrev))
 
+(defun ora-company-number ()
+  "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (cl-find-if (lambda (s) (string-match re s))
+                    company-candidates)
+        (self-insert-command 1)
+      (company-complete-number
+       (if (equal k "0")
+           10
+         (string-to-number k))))))
+
 (let ((map company-active-map))
-  (mapc (lambda (x) (define-key map (format "%d" x)
-                 `(lambda () (interactive) (company-complete-number ,x))))
+  (mapc (lambda (x) (define-key map (format "%d" x) 'ora-company-number))
         (number-sequence 0 9))
-  (define-key map " " (lambda () (interactive)
-                         (company-abort)
-                         (self-insert-command 1)))
+  (define-key map " " (lambda ()
+                        (interactive)
+                        (company-abort)
+                        (self-insert-command 1)))
   (define-key map (kbd "<return>") nil))
 
 (provide 'ora-company)
