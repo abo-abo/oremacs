cf83ee2c972df20c3acd960c3e62d5659266d051
Oleh Krehel <ohwoeowho@gmail.com>
2018-03-25 14:45:19 +0200

    modes/ora-dired.el (dired-omit-files): Update

--- modes/ora-dired.el
+++ modes/ora-dired.el
@@ -20,7 +20,7 @@
       (format "\\(?:\\.%s\\'\\)\\|%s\\|\\`\\.[^.]\\|\\`_minted"
               (regexp-opt
                '("aux" "log" "pickle" "synctex.gz" "run.xml" "bcf" "am" "in" "blx.bib"
-                 "vrb" "opt" "nav" "snm" "out"))
+                 "vrb" "opt" "nav" "snm" "out" "ass"))
               (regexp-opt
                '("compile_commands.json"
                  "TAGS"
