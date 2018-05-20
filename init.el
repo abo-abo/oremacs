2a84cf5ff24f9570d301772d0e98959753154458
Oleh Krehel <ohwoeowho@gmail.com>
2018-05-20 23:25:42 +0200

    init.el: Update

--- init.el
+++ init.el
@@ -326,6 +326,7 @@
       (require 'slime-autoloads)
       (setq slime-contribs '(slime-fancy))
       (setq inferior-lisp-program "/usr/bin/sbcl")))
+(require 'cook)
 (use-package elf-mode
     :commands elf-mode
     :init
