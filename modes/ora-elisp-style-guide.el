719406fba67d7b7f5ce0f0e0ce768acfa98ebc5c
Oleh Krehel <ohwoeowho@gmail.com>
2018-08-06 20:34:40 +0200

    modes/ora-elisp-style-guide.el: Update

--- modes/ora-elisp-style-guide.el
+++ modes/ora-elisp-style-guide.el
@@ -3,6 +3,7 @@
 ;; (setq lisp-indent-function 'lisp-indent-function)
 (setq lisp-indent-function 'common-lisp-indent-function)
 
+(put 'defalias 'common-lisp-indent-function 1)
 (put 'if 'common-lisp-indent-function 2)
 (put 'defface 'common-lisp-indent-function 1)
 (put 'define-minor-mode 'common-lisp-indent-function 1)
