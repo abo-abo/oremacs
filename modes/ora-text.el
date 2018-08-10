dcec94f508d7b1d0a5682c4608b37b913fc61718
Oleh Krehel <ohwoeowho@gmail.com>
2018-08-10 18:10:22 +0200

    modes/ora-text.el: Update

--- modes/ora-text.el
+++ modes/ora-text.el
@@ -1,4 +1,22 @@
 (define-key text-mode-map (kbd "C-c C-c") 'server-edit)
+(define-key text-mode-map (kbd "<M-up>") 'ora-move-line-up)
+(define-key text-mode-map (kbd "<M-down>") 'ora-move-line-down)
+
+(defun ora-move-line-up ()
+  (interactive)
+  (save-excursion
+    (transpose-lines 1))
+  (forward-line -1))
+
+(defun ora-move-line-down ()
+  (interactive)
+  (let ((pt (point)))
+    (forward-line 1)
+    (if (eobp)
+        (goto-char pt)
+      (transpose-lines 1)
+      (goto-char pt)
+      (forward-line 1))))
 
 ;;;###autoload
 (defun ora-text-hook ()
