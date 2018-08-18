c0b180ad263fe5a6c71f4b3870c00d926bcb1d09
Oleh Krehel <ohwoeowho@gmail.com>
2018-08-18 12:12:13 +0200

    modes/ora-dired.el: Update

--- modes/ora-dired.el
+++ modes/ora-dired.el
@@ -249,6 +249,8 @@ Number of marked items: %(length (dired-get-marked-files))
              '("\\.7z\\'" "" "7z x '%i'"))
 (add-to-list 'dired-compress-file-suffixes
              '("\\.tgz\\'" "" "gzip -dc %i | tar -xv"))
+(add-to-list 'dired-compress-file-suffixes
+             '("\\.tar\\'" "" "tar -xf %i"))
 
 (eval-after-load 'tramp-sh
   '(progn
