cf8fa93e7860a7bd2d6467ad29a781e51d673c9a
Oleh Krehel <ohwoeowho@gmail.com>
2018-10-17 19:01:32 +0200

    modes/ora-compilation.el: Update

--- modes/ora-compilation.el
+++ modes/ora-compilation.el
@@ -40,3 +40,28 @@
              face compilation-info-modeline
              help-echo "Number of informational messages so far")
         "]"))
+
+(defun ora-compilation-exit-message-function (process-status exit-status msg)
+  (when (eq major-mode 'compilation-mode)
+    (let* ((start-time-str (save-excursion
+                             (goto-char (point-min))
+                             (and (re-search-forward "^Compilation started at \\(.*\\)" nil t)
+                                  (match-string 1))))
+           (start-time-arr (parse-time-string start-time-str))
+           (start-time (encode-time (nth 0 start-time-arr)
+                                    (nth 1 start-time-arr)
+                                    (nth 2 start-time-arr)
+                                    (nth 3 start-time-arr)
+                                    (nth 4 start-time-arr)
+                                    (or (nth 5 start-time-arr)
+                                        (nth 5 (decode-time (current-time))))))
+           (end-time (current-time))
+           (seconds-passed (time-to-seconds
+                            (time-subtract end-time start-time))))
+      (when (or (> seconds-passed 60)
+                (not (member (current-buffer) (mapcar 'window-buffer (window-list)))))
+        (pop-to-buffer (current-buffer))
+        (shell-command "play ~/Documents/tng-doorbell.wav"))))
+  (cons msg exit-status))
+
+(setq compilation-exit-message-function 'ora-compilation-exit-message-function)
