76b623d3e177b6c7e74201bd0a4f89a2da328efd
Oleh Krehel <ohwoeowho@gmail.com>
2018-12-23 18:15:02 +0200

    modes/ora-ruby.el: Update

--- modes/ora-ruby.el
+++ modes/ora-ruby.el
@@ -13,7 +13,6 @@
 ;;;###autoload
 (defun ora-ruby-hook ()
   (setq-local tab-width 2)
-  ;; (electric-spacing-mode 1)
   (add-hook 'local-write-file-hooks
             (lambda ()
               (save-excursion
