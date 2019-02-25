8b5597f8e321cdd5809f046ab03248b7f8904f41
Oleh Krehel <ohwoeowho@gmail.com>
2019-02-25 18:38:35 +0100

    modes/ora-dired.el: Use counsel--call

--- modes/ora-dired.el
+++ modes/ora-dired.el
@@ -88,9 +88,7 @@
 (defun ora-dired-show-octal-permissions ()
   "Show current item premissons, e.g. for later use in chmod."
   (interactive)
-  (let ((r (shell-command-to-string
-            (concat "stat -c \"%a %n\" "
-                    (shell-quote-argument (car (dired-get-marked-files)))))))
+  (let ((r (counsel--call "stat" "-c" "%a %n" (car (dired-get-marked-files)))))
     (message (car (split-string r " ")))))
 
 (defun ora-dired-get-size ()
