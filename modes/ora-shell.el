b1b3905bf69593be38abd0c6740c0bfdb07f3554
Oleh Krehel <ohwoeowho@gmail.com>
2019-03-24 15:55:29 +0100

    modes/ora-shell.el (ora-ansi-color-apply-on-region): Add

--- modes/ora-shell.el
+++ modes/ora-shell.el
@@ -4,4 +4,20 @@
 ;;;###autoload
 (defun ora-shell-hook ())
 
+(advice-add 'ansi-color-apply-on-region :before 'ora-ansi-color-apply-on-region)
+
+(defun ora-ansi-color-apply-on-region (begin end)
+  "Fix progress bars for e.g. apt(8).
+Display progress in the mode line instead."
+  (let ((end-marker (copy-marker end)))
+    (save-excursion
+      (goto-char begin)
+      (while (re-search-forward "\0337\\(.*\\)\0338" end-marker t)
+        (let ((progress (delete-and-extract-region (match-beginning 0) (point))))
+          (setq mode-line-process
+                (if (string-match "Progress: \\[ *\\([0-9]+\\)%\\]" progress)
+                    (list (concat ":%s " (match-string 1 progress) "%%%% "))
+                  '(":%s")))
+          (force-mode-line-update))))))
+
 (provide 'ora-shell)
