342dedaa45d85b49292793af21f713e96cb21e1b
Oleh Krehel <ohwoeowho@gmail.com>
2019-03-25 10:52:50 +0100

    modes/ora-shell.el (ora-ansi-color-apply-on-region): Refactor

--- modes/ora-shell.el
+++ modes/ora-shell.el
@@ -9,15 +9,27 @@
 (defun ora-ansi-color-apply-on-region (begin end)
   "Fix progress bars for e.g. apt(8).
 Display progress in the mode line instead."
-  (let ((end-marker (copy-marker end)))
+  (let ((end-marker (copy-marker end))
+        mb)
     (save-excursion
-      (goto-char begin)
-      (while (re-search-forward "\0337\\(.*\\)\0338" end-marker t)
-        (let ((progress (delete-and-extract-region (match-beginning 0) (point))))
-          (setq mode-line-process
-                (if (string-match "Progress: \\[ *\\([0-9]+\\)%\\]" progress)
-                    (list (concat ":%s " (match-string 1 progress) "%%%% "))
-                  '(":%s")))
-          (force-mode-line-update))))))
+      (goto-char (copy-marker begin))
+      (while (re-search-forward "\0337" end-marker t)
+        (setq mb (match-beginning 0))
+        (when (re-search-forward "\0338" end-marker t)
+          (let ((progress (buffer-substring-no-properties
+                           (+ mb 2) (- (point) 2))))
+            (delete-region mb (point))
+            (ora-apt-progress-message progress)))))))
+
+(defun ora-apt-progress-message (progress)
+  ;; (setq mode-line-process
+  ;;       (if (string-match "Progress: \\[ *\\([0-9]+\\)%\\]" progress)
+  ;;           (list (concat ":%s " (match-string 1 progress) "%%%% "))
+  ;;         '(":%s")))
+  ;; (force-mode-line-update)
+  (message
+   (replace-regexp-in-string
+    "%" "%%"
+    (ansi-color-apply progress))))
 
 (provide 'ora-shell)
