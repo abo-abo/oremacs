0791751626935ea6e6b691720cc5f5dd75269a10
Oleh Krehel <ohwoeowho@gmail.com>
2019-04-06 20:59:34 +0200

    modes/ora-mu4e.el: Update

--- modes/ora-mu4e.el
+++ modes/ora-mu4e.el
@@ -1,17 +1,13 @@
-(add-to-list 'load-path "~/git/mu/mu4e")
-(require 'mu4e)
-(setq mu4e-maildir "~/mail/gmail")
+(require 'whicher)
+(require 'smime)
+(use-package mu4e
+    :load-path "~/git/mu/mu4e")
 (setq mu4e-maildir "~/mail/work")
-
-(setq mu4e-html2text-command "w3m -T text/html")
-
+(setq mu4e-html2text-command (whicher "w3m -T text/html"))
 (setq mu4e-drafts-folder "/Drafts")
-(setq mu4e-sent-folder   "/Sent Items")
-(setq mu4e-trash-folder  "/Trash")
-(setq mu4e-headers-skip-duplicates t)
-;; don't save message to Sent Messages, Gmail/IMAP takes care of this
-(setq mu4e-sent-messages-behavior 'delete)
-(setq mu4e-get-mail-command "mbsync -a")
+(setq mu4e-sent-folder "/Sent Items")
+(setq mu4e-trash-folder "/Trash")
+(setq mu4e-get-mail-command (whicher "mbsync -a"))
 (setq message-kill-buffer-on-exit t)
 (setq mu4e-confirm-quit nil)
 (setq mu4e-headers-date-format "%d-%m-%Y %H:%M")
@@ -26,31 +22,70 @@
 (setq mu4e-change-filenames-when-moving t)
 (setq message-sendmail-f-is-evil nil)
 
+;; don't save message to Sent Messages, IMAP server takes care of that
+(setq mu4e-sent-messages-behavior 'delete)
+
 ;; setup some handy shortcuts
 ;; you can quickly switch to your Inbox -- press ``ji''
 ;; then, when you want archive some messages, move them to
 ;; the 'All Mail' folder by pressing ``ma''.
 (setq mu4e-maildir-shortcuts
       '(("/INBOX" . ?i)
-        ("/[Gmail].Sent Mail" . ?s)
-        ("/[Gmail].Trash" . ?t)
-        ("/[Gmail].All Mail" . ?a)
-        ("/[asml].All Mail" . ?m)))
+        ("/Archive" . ?a)
+        ("/Trash" . ?t)))
 
-;; TODO
-;; (define-key mu4e-view-mode-map "j" 'forward-paragraph)
-;; (define-key mu4e-view-mode-map "k" 'backward-paragraph)
-;; (define-key mu4e-headers-mode-map "h" 'mu4e-headers-prev)
-(require 'smtpmail)
+(define-key mu4e-compose-mode-map (kbd "C-M-i") nil)
+(define-key mu4e-headers-mode-map "J" 'mu4e-headers-next)
+(define-key mu4e-headers-mode-map "K" 'mu4e-headers-prev)
+(define-key mu4e-view-mode-map "J" 'mu4e-view-headers-next)
+(define-key mu4e-view-mode-map "K" 'mu4e-view-headers-prev)
+(define-key mu4e-view-mode-map (kbd "C--") nil)
 
 ;;;###autoload
 (defun ora-mu4e-headers-hook ())
-(define-key mu4e-view-mode-map (kbd "C--") nil)
-(define-key mu4e-compose-mode-map (kbd "C-M-i") nil)
 
 ;;;###autoload
 (defun ora-mu4e-compose-hook ()
+  (setq truncate-lines t)
   (setq completion-at-point-functions
         '(mu4e~compose-complete-contact t)))
+
+(setq mm-7bit-chars "\x20-\x7f\n\r\t\x7\x8\xb\xc\x1f")
+(setq message-send-hook '(ora-smime-sign))
+
+(defun ora-smime-sign ()
+  (let* ((from (message-field-value "From"))
+         (keyfile
+          (cadr
+           (cl-find-if
+            (lambda (x)
+              (string-match (car x) from))
+            smime-keys))))
+    (when keyfile
+      (let ((openssl-args
+             (list "smime" "-sign" "-signer" keyfile
+                   ;; openssl will add "Content-Type: text/plain"
+                   "-text")))
+        (if (string-match-p "plain.pem$" keyfile)
+            (apply #'call-process-region
+                   (message-goto-body) (point-max) smime-openssl-program t t nil
+                   openssl-args)
+          (let ((passphrase (smime-ask-passphrase keyfile)))
+            (unwind-protect
+                 (progn
+                   (setenv "GNUS_SMIME_PASSPHRASE" passphrase)
+                   (apply #'call-process-region
+                          (message-goto-body) (point-max) smime-openssl-program t t nil
+                          (append openssl-args '("-passin" "env:GNUS_SMIME_PASSPHRASE"))))
+              (setenv "GNUS_SMIME_PASSPHRASE" "")))))
+      ;; we already specify MIME-Version here, don't let
+      ;; `message-encode-message-body' mess it up.
+      (setq message-inhibit-body-encoding t)
+      (message-goto-body)
+      ;; move MIME-Version to headers
+      (when (looking-at "MIME-Version: 1.0\nContent-Type.*\n")
+        (let ((mime (delete-and-extract-region (point) (match-end 0))))
+          (re-search-backward "^--text follows this line--" nil t)
+          (insert mime))))))
 
 (provide 'ora-mu4e)
