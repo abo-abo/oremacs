018d7d5e4cab8a1ae0dc668ebb57973ea806f256
Oleh Krehel <ohwoeowho@gmail.com>
2019-07-20 22:24:36 +0200

    modes/ora-sml.el: Update

--- modes/ora-sml.el
+++ modes/ora-sml.el
@@ -1,9 +1,12 @@
 (require 'sml-mode)
 (require 'soap)
 
-
 ;;;###autoload
 (defun ora-sml-hook ())
 
 (dolist (k '("=" "+" "-" "<" ">" ","))
   (define-key sml-mode-map k 'soap-command))
+
+(require 'pora-sml nil t)
+
+(provide 'ora-sml)
