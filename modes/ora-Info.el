21cf3bd416533db49b07205a2c2abb1d6b5859bf
Oleh Krehel <ohwoeowho@gmail.com>
2019-07-22 17:50:02 +0200

    modes/ora-Info.el: Update

--- modes/ora-Info.el
+++ modes/ora-Info.el
@@ -2,19 +2,8 @@
 (setq Info-additional-directory-list
       (list (expand-file-name "etc/info/" emacs-d)))
 
-(add-to-list 'Info-directory-list "/usr/local/share/info/")
-
-(define-key Info-mode-map "j" 'ora-para-down)
-(define-key Info-mode-map "k" 'ora-para-up)
-(define-key Info-mode-map "v" 'recenter-top-bottom)
-(define-key Info-mode-map "h" 'backward-char)
-(define-key Info-mode-map "l" 'forward-char)
 (define-key Info-mode-map "w" 'forward-word)
 (define-key Info-mode-map "b" 'backward-word)
-(define-key Info-mode-map "a" 'beginning-of-line)
-(define-key Info-mode-map "e" 'end-of-line)
-(define-key Info-mode-map "A" 'beginning-of-buffer)
-(define-key Info-mode-map "E" 'end-of-buffer)
 (define-key Info-mode-map "t" 'hydra-info-to/body)
 (define-key Info-mode-map "u" 'Info-history-back)
 (define-key Info-mode-map "c" 'counsel-ace-link)
@@ -39,3 +28,5 @@ _o_rg e_l_isp _e_macs _h_yperspec"
   ("l" (ora-open-info "elisp" "*elisp info*"))
   ("e" (ora-open-info "emacs" "*emacs info*"))
   ("h" (ora-open-info "gcl" "*hyperspec*")))
+
+(require 'pora-Info nil t)
