041b200d1cf5ba2ed271ca83d03fa39fc4b4a573
Oleh Krehel <ohwoeowho@gmail.com>
2019-10-21 11:40:49 +0200

    hooks.el: Update

--- hooks.el
+++ hooks.el
@@ -84,7 +84,9 @@
 (eval-after-load 'grep
   '(define-key grep-mode-map (kbd "C-x C-q") 'wgrep-change-to-wgrep-mode))
 (eval-after-load 'wgrep
-  '(define-key grep-mode-map (kbd "C-c C-c") 'wgrep-finish-edit))
+  '(progn
+    (define-key grep-mode-map (kbd "C-c C-c") 'wgrep-finish-edit)
+    (setq wgrep-auto-save-buffer t)))
 (eval-after-load 'erc
   '(require 'ora-erc))
 (eval-after-load 'elfeed-search
