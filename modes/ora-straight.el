828a4f60b21db4e28f2a032dc4a3edeca045b1de
Oleh Krehel <ohwoeowho@gmail.com>
2020-02-19 18:47:46 +0100

    modes/ora-straight.el: Update

--- modes/ora-straight.el
+++ modes/ora-straight.el
@@ -2,10 +2,19 @@
 ;; (setq straight-use-package-by-default t)
 ;; (straight-use-package 'use-package)
 
-(let ((build-dir "~/.emacs.d/straight/build/"))
-  (dolist (pkg (delete "cl-lib" (delete ".." (delete "." (directory-files build-dir)))))
-    (let ((dir (expand-file-name pkg build-dir)))
-      (add-to-list 'load-path dir)
-      (load (car (directory-files dir t "-autoloads.el")) t 'nomessage))))
+(require 'cl-seq)
+(cl-reduce (lambda (a b)) '(0 0))
+
+(defun straight-reload-all ()
+  (interactive)
+  (let ((build-dir "~/.emacs.d/straight/build/"))
+    (dolist (pkg (delete "cl-lib" (delete ".." (delete "." (directory-files build-dir)))))
+      (let* ((dir (expand-file-name pkg build-dir))
+             (autoloads (car (directory-files dir t "-autoloads.el"))))
+        (add-to-list 'load-path dir)
+        (when autoloads
+          (load autoloads t 'nomessage))))))
+
+(straight-reload-all)
 
 (provide 'ora-straight)
