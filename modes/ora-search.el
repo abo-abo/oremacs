f28cc3272ca30f8d6660ea2ba746800db3f76569
Oleh Krehel <ohwoeowho@gmail.com>
2020-03-07 20:35:35 +0100

    modes/ora-search.el: Update

--- modes/ora-search.el
+++ modes/ora-search.el
@@ -9,6 +9,7 @@
   ("m" (ora-search-query "https://www.google.com/maps/search/%s?hl=en&source=opensearch") "maps")
   ("r" (ora-search-query "https://www.reddit.com/search?q=%s") "reddit")
   ("t" (ora-search-query "https://twitter.com/search?q=%s") "twitter")
+  ("T" (browse-url "https://www.google.com/travel") "travel")
   ("y" (ora-search-query "https://www.youtube.com/results?search_query=%s&page={startPage?}&utm_source=opensearch") "youtube")
   ("q" nil "quit"))
 
