ac625d93cae11104e4b5049ff255a707dfe94f9f
Oleh Krehel <ohwoeowho@gmail.com>
2020-06-14 21:50:23 +0200

    modes/ora-haskell.el: Update

--- modes/ora-haskell.el
+++ modes/ora-haskell.el
@@ -1,8 +1,15 @@
 (require 'haskell)
 (require 'lpy-soap)
 (require 'company-dabbrev)
+
+(defun ora-haskell-comment-insert ()
+  (insert "-- "))
+
 ;;;###autoload
-(defun ora-haskell-hook ())
+(defun ora-haskell-hook ()
+  (setq comment-insert-comment-function #'ora-haskell-comment-insert)
+  (company-mode))
+
 
 (require 'pora-haskell nil t)
 
