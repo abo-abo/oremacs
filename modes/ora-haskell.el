b6b553bf8937834edc4351f599428bce32a553bd
Oleh Krehel <ohwoeowho@gmail.com>
2020-06-16 19:15:36 +0200

    modes/ora-haskell.el: Update

--- modes/ora-haskell.el
+++ modes/ora-haskell.el
@@ -5,9 +5,21 @@
 (defun ora-haskell-comment-insert ()
   (insert "-- "))
 
+(defun ora-haskell-completion-at-point ()
+  (let* ((bnd (bounds-of-thing-at-point 'symbol))
+         (prefix (buffer-substring-no-properties (car bnd) (cdr bnd)))
+         (cands (save-excursion
+                  (delete-dups
+                   (all-completions
+                    prefix
+                    (company-dabbrev--search "\\(?:\\sw\\)+" company-dabbrev-time-limit 'all))))))
+    (when cands
+      (list (car bnd) (cdr bnd) cands))))
+
 ;;;###autoload
 (defun ora-haskell-hook ()
   (setq comment-insert-comment-function #'ora-haskell-comment-insert)
+  (cl-pushnew #'ora-haskell-completion-at-point completion-at-point-functions)
   (company-mode))
 
 
