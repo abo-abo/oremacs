45084ca9c8e9f16bdd485ec749e6c68e62b9efed
Oleh Krehel <ohwoeowho@gmail.com>
2020-11-06 22:14:05 +0100

    modes/ora-dired.el (ora-dired-show-octal-permissions): Work on remote

--- modes/ora-dired.el
+++ modes/ora-dired.el
@@ -68,7 +68,11 @@
 (defun ora-dired-show-octal-permissions ()
   "Show current item premissons, e.g. for later use in chmod."
   (interactive)
-  (let ((r (counsel--command "stat" "-c" "%a %n" (car (dired-get-marked-files)))))
+  (let ((r (counsel--command
+            "stat" "-c" "%a %n"
+            (tramp-file-name-localname
+             (tramp-dissect-file-name
+              (car (dired-get-marked-files)))))))
     (message (car (split-string r " ")))))
 
 (defun ora-dired-get-size ()
