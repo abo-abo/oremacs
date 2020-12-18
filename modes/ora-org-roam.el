cbdacddffdd44bfa11f47fe287f8fe3f916623d9
Oleh Krehel <ohwoeowho@gmail.com>
2020-12-18 23:05:45 +0100

    modes/ora-org-roam.el (ora-org-roam-find-file): An ivy version of org-roam-find-file

--- modes/ora-org-roam.el
+++ modes/ora-org-roam.el
@@ -25,8 +25,28 @@
 (defhydra hydra-org-roam (:exit t :idle 0.8)
   "Launcher for `org-roam'."
   ("i" org-roam-insert "insert")
-  ("f" org-roam-find-file "find-file"))
+  ("f" ora-org-roam-find-file "find-file"))
 
 (org-roam-mode)
+
+(defun ora-org-roam-find-file-action (x)
+  (let ((title-with-tags (car x))
+        (file-path (plist-get (cdr x) :path)))
+    (if file-path
+        (org-roam--find-file file-path)
+      (let ((org-roam-capture--info
+             `((title . ,title-with-tags)
+               (slug . ,(funcall org-roam-title-to-slug-function title-with-tags))))
+            (org-roam-capture--context 'title))
+        (setq org-roam-capture-additional-template-props (list :finalize 'find-file))
+        (org-roam-capture--capture))))
+  (message "%S" x))
+
+(defun ora-org-roam-find-file ()
+  (interactive)
+  (unless org-roam-mode (org-roam-mode))
+  (ivy-read "File: " (org-roam--get-title-path-completions)
+            :action #'ora-org-roam-find-file-action
+            :caller 'ora-org-roam-find-file))
 
 (provide 'ora-org-roam)
