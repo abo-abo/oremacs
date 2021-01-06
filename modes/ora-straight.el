90df18373d23d8bc8596afe3cc0dd63974a0675a
Oleh Krehel <ohwoeowho@gmail.com>
2021-01-06 23:25:16 +0100

    modes/ora-straight.el: Update

--- modes/ora-straight.el
+++ modes/ora-straight.el
@@ -5,11 +5,21 @@
 (require 'cl-seq)
 (cl-reduce (lambda (a b)) '(0 0))
 
+(defun ora-directory-files (dir)
+  (delete ".." (delete "." (directory-files dir))))
+
 ;;;###autoload
 (defun ora-straight-reload-all ()
   (interactive)
-  (let ((build-dir "~/.emacs.d/straight/build/"))
-    (dolist (pkg (delete "cl-lib" (delete ".." (delete "." (directory-files build-dir)))))
+  (let* ((build-dir "~/.emacs.d/straight/build/")
+         (emacs-git (expand-file-name "git/" emacs-d))
+         (git-files (ora-directory-files emacs-git))
+         (pkgs (cl-set-difference
+                (delete "cl-lib" (ora-directory-files build-dir))
+                (append '("cl-lib" "counsel" "ivy" "ivy-avy")
+                        git-files)
+                :test #'equal)))
+    (dolist (pkg pkgs)
       (let* ((dir (expand-file-name pkg build-dir))
              (autoloads (car (directory-files dir t "-autoloads.el"))))
         (add-to-list 'load-path dir)
