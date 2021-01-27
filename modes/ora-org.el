02ea3261cfc1b6820c18e892cc9d13a95e4473ce
Oleh Krehel <ohwoeowho@gmail.com>
2021-01-27 21:42:36 +0100

    modes/ora-org.el: Add LIST

--- modes/ora-org.el
+++ modes/ora-org.el
@@ -147,13 +147,14 @@
 ;;* Basic settings
 (setq-default org-todo-keywords
               '((sequence "TODO"
-                 "WAITING" "PAUSED"
+                 "WAITING" "PAUSED" "LIST"
                  ;; "NEXT"
                  "|" "DONE" "CANCELLED")))
 (setq org-todo-keyword-faces
       '(("TODO" . (:foreground "red" :weight bold))
         ("WAITING" . (:foreground "blue" :weight bold))
-        ("PAUSED" . (:foreground "orange" :weight bold))))
+        ("PAUSED" . (:foreground "orange" :weight bold))
+        ("LIST" . (:foreground "orange" :weight bold))))
 (setq org-startup-indented t)
 (setq org-startup-folded nil)
 (setq org-cycle-separator-lines 0)
