5726f5be9ef53f32d994592d694bc91d200c14b7
Oleh Krehel <ohwoeowho@gmail.com>
2021-03-28 19:40:08 +0200

    modes/ora-org-roam.el: Update

--- modes/ora-org-roam.el
+++ modes/ora-org-roam.el
@@ -1,6 +1,6 @@
+(require 'pora-org nil t)
 (use-package org-roam
   :diminish org-roam-mode)
-(require 'ora-org-journal)
 (require 'pamparam)
 (setq wgrep-colon-file-separator-header-regexp "\\(?1:[^\n]+?[^\n/:]\\):\\(?3:[1-9][0-9]*\\)[	 ]*:")
 
@@ -12,7 +12,7 @@
         (directory-file-name
          plain-org-wiki-directory))))
 
-(org-roam-mode)
+;; (org-roam-mode)
 (require 'org-roam-protocol)
 (setq org-roam-buffer-position 'bottom)
 (setq org-roam-completion-system 'ivy)
