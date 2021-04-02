6f3c4f897e70253b54973edde42ab3370709bf4b
Oleh Krehel <ohwoeowho@gmail.com>
2021-04-02 18:58:57 +0200

    modes/ora-org-journal.el (org-journal-date-format): Customize

--- modes/ora-org-journal.el
+++ modes/ora-org-journal.el
@@ -1,7 +1,8 @@
 (use-package org-journal
   :config
   (setq org-journal-dir (expand-file-name "../journal/" plain-org-wiki-directory))
-  (setq org-journal-file-format "%Y-%m-%d"))
+  (setq org-journal-file-format "%Y-%m-%d")
+  (setq org-journal-date-format "%A, %e %b %Y"))
 
 (defhydra hydra-org-journal (:exit t :idle 0.8)
   "Launcher for `org-journal'."
