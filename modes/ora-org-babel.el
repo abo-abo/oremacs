2d67479fbc5aaff282821107c9e81ecea489b78a
Oleh Krehel <ohwoeowho@gmail.com>
2021-06-29 19:26:29 +0200

    modes/ora-org-babel.el: Add font lock for inline source code

--- modes/ora-org-babel.el
+++ modes/ora-org-babel.el
@@ -63,5 +63,15 @@
 ;; (setf (cdr (assoc :results org-babel-default-header-args)) "output silent")
 (setf (cdr (assoc :noweb org-babel-default-header-args)) "yes")
 
+(font-lock-add-keywords
+ 'org-mode
+ '(("\\(src_\\)\\([^[{]+\\)\\(\\(?:\\[:.*\\]\\)?\\){\\([^}]*\\)}"
+    ;; src
+    (1 'default)
+    ;; lang
+    (2 'default)
+    ;; headers
+    (3 'font-lock-comment-face)
+    (4 'org-code))))
 
 (provide 'ora-org-babel)
