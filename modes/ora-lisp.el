9cd924642c3cfcd56b6ced9ef1c7781b6986ff94
Oleh Krehel <ohwoeowho@gmail.com>
2021-11-15 19:18:16 +0100

    modes/ora-lisp.el: Update

--- modes/ora-lisp.el
+++ modes/ora-lisp.el
@@ -18,12 +18,18 @@
   (when (bound-and-true-p slime-autodoc-mode)
     (slime-autodoc-mode -1)))
 
+;; (eval-after-load 'slime-autodoc
+;;   '(setf (symbol-function 'slime-autodoc-space) #'self-insert-command))
+
 (defun ora-slime-completion-in-region (_fn completions start end)
   (funcall completion-in-region-function start end completions))
 
 (advice-add
  'slime-display-or-scroll-completions
  :around #'ora-slime-completion-in-region)
+
+(lispy-raise-minor-mode 'lispy-mode)
+(lispy-raise-minor-mode 'lispy-other-mode)
 
 (require 'pora-lisp nil t)
 (provide 'ora-lisp)
