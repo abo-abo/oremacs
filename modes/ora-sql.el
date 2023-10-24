3a1356a4116490de2a2c0204a251912be5f65a31
Oleh Krehel <ohwoeowho@gmail.com>
2023-10-24 18:32:08 +0100

    modes/ora-sql.el (ora-sql-string-linebreak): Add and bind to "M-j"

--- modes/ora-sql.el
+++ modes/ora-sql.el
@@ -3,10 +3,20 @@
 ;;;###autoload
 (defun ora-sql-hook ()
   (sqlind-minor-mode)
-  (company-mode)
   ;; setup escape character
   (modify-syntax-entry ?\\ "\\" sql-mode-syntax-table)
   (modify-syntax-entry ?\\ "\\" sqlind-syntax-table))
+
+(defun ora-sql-string-linebreak ()
+  (interactive)
+  (cond ((lispy--in-string-p)
+         (insert "' ||")
+         (newline-and-indent)
+         (insert "'"))
+        (t
+         (default-indent-new-line))))
+
+(define-key sql-mode-map (kbd "M-j") 'ora-sql-string-linebreak)
 
 (require 'pora-sql nil t)
 (provide 'ora-sql)
