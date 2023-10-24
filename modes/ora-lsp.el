c5d666edc291f7bc3989133e5ca665c175bb3c25
Oleh Krehel <ohwoeowho@gmail.com>
2023-10-24 18:47:54 +0100

    modes/ora-lsp.el: Update

--- modes/ora-lsp.el
+++ modes/ora-lsp.el
@@ -1,8 +1,22 @@
+(setq lsp-auto-configure nil)
+(setq lsp-warn-no-matched-clients nil)
 (require 'lsp)
+(require 'lsp-ui)
+;; `lsp--auto-configure'
+(setq lsp-headerline-breadcrumb-enable nil)
+(setq lsp-modeline-code-actions-enable nil)
+(setq lsp-modeline-diagnostics-enable nil)
+(setq lsp-modeline-workspace-status-enable nil)
+(setq lsp-diagnostics-provider nil)
 
 (setq lsp-auto-guess-root t)
-(setq lsp-headerline-breadcrumb-enable nil)
+
+(setq lsp-ui-doc-position 'at-point)
+(define-key lsp-ui-mode-map (kbd "C-1") 'lsp-ui-doc-toggle)
+
+(setq lsp-headerline-breadcrumb-enable-diagnostics nil)
 (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+(add-hook 'lsp-configure-hook 'lsp-completion--enable)
 
 (global-set-key (kbd "C-<") lsp-command-map)
 
@@ -15,4 +29,5 @@
 
 (setq lsp-clients-clangd-executable "/usr/bin/clangd-8")
 
+(require 'pora-lsp nil t)
 (provide 'ora-lsp)
