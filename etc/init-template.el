3cee1437e8d13f717b10652d2f489daca409fbe6
Oleh Krehel <ohwoeowho@gmail.com>
2025-01-21 18:18:32 +0000

    etc/init-template.el: Simplify

--- etc/init-template.el
+++ etc/init-template.el
@@ -5,18 +5,12 @@
 ;;* Org mode
 (defvar ora-org-basedir
   (expand-file-name "org/" emacs-d)
-  "Org dir should contain: gtd.org, ent.org, diary, and wiki/.")
+  "Org dir should contain: gtd.org, inbox.org")
 
 (defun ora-org-expand (file)
   (expand-file-name file ora-org-basedir))
 (setq org-agenda-files
-      (mapcar #'ora-org-expand '("gtd.org" "ent.org")))
+      (mapcar #'ora-org-expand '("gtd.org" "inbox.org")))
 
-;; org-mode wiki
-(use-package plain-org-wiki
-  :commands plain-org-wiki plain-org-wiki-helm
-  :config
-  (setq plain-org-wiki-directory
-        (ora-org-expand "wiki/")))
 
 (provide 'personal-init)
