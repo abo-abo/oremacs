4a243d9ee757bb3549e7a07615ebc17c8425a03a
Oleh Krehel <ohwoeowho@gmail.com>
2025-02-13 18:20:03 +0000

    modes/ora-comint.el (ora-comint-goto-link): Add and bind to "M-."

--- modes/ora-comint.el
+++ modes/ora-comint.el
@@ -2,6 +2,8 @@
 
 (define-key comint-mode-map (kbd "<tab>") 'completion-at-point)
 (define-key comint-mode-map (kbd "C-k") 'ora-comint-kill-line)
+(define-key comint-mode-map (kbd "M-.") 'ora-comint-goto-link)
+
 
 ;;;###autoload
 (defun ora-comint-hook ()
@@ -13,6 +15,7 @@
           shell-c-a-p-replace-by-expanded-directory
           shell-filename-completion
           comint-filename-completion))
+  (make-variable-buffer-local 'comint-preoutput-filter-functions)
   (shell-dirtrack-mode 1))
 
 (defun ora-fontify-shell-links (str)
@@ -46,5 +49,28 @@
         (kill-region
          (point) (+ (line-beginning-position) (cdr bnd) -2))
       (kill-region (point) (line-end-position)))))
+
+(defun ora-comint-goto-link ()
+  (interactive)
+  (let (fname line)
+    (when (cond ((save-excursion
+                   (beginning-of-line)
+                   (looking-at "\\(/?\\)\\(.*\\):\\([0-9]+\\):"))
+                 (setq root-p (match-string-no-properties 1))
+                 (setq fname (match-string-no-properties 2))
+                 (setq line (match-string-no-properties 3))
+                 (if (string= root-p "")
+                     (setq fname (expand-file-name fname))
+                   (setq fname (concat root-p fname))))
+                ((save-excursion
+                   (beginning-of-line)
+                   (re-search-forward "lineno=\\([0-9]+\\).*pathname=\\(.*\\) pid" (line-end-position) t))
+                 (setq line (match-string-no-properties 1))
+                 (setq fname (match-string-no-properties 2))))
+      (xref-push-marker-stack)
+      (find-file fname)
+      (goto-char (point-min))
+      (forward-line (1- (string-to-number line)))
+      (lpy-beginning-of-line))))
 
 (provide 'ora-comint)
