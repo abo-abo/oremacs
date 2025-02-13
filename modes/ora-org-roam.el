4e159ea774331fa6248e1adfdff4ad31de54da19
Oleh Krehel <ohwoeowho@gmail.com>
2025-02-13 18:21:08 +0000

    modes/ora-org-roam.el: Update

--- modes/ora-org-roam.el
+++ modes/ora-org-roam.el
@@ -4,12 +4,7 @@
 (setq wgrep-colon-file-separator-header-regexp "\\(?1:[^\n]+?[^\n/:]\\):\\(?3:[1-9][0-9]*\\)[	 ]*:")
 
 (setq org-roam-verbose nil)
-(setq org-roam-directory
-      (expand-file-name
-       "roam"
-       (file-name-directory
-        (directory-file-name
-         plain-org-wiki-directory))))
+(setq org-roam-directory (expand-file-name "roam" org-directory))
 
 ;; (org-roam-mode)
 (require 'org-roam-protocol)
